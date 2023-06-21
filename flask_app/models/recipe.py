e299261f5fc5850b75f81a53cae9c6da3762af5e
Kenny Weiss <kenny1weiss@aol.com>
2023-06-21 11:30:33 -0700

    Add recipe class

---
+++ flask_app/models/recipe.py
@@ -0,0 +1,17 @@
+from flask import flash
+from flask_app.config.mysqlconnection import connectToMySQL
+
+class Recipe:
+    db = "family_recipes"
+    def __init__(self, data):
+        self.id = data['id']
+        self.title = data['title']
+        self.ingredients = data['ingredients']
+        self.img = data['img']
+        self.description = data['description']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+        self.users_id = data['users_id']
+
+    @classmethod
+    def create(cls,data):
