424930a486e0b5f721a2b4d1caee4022e7207e52
Christian Johnson <charcoaldust@mac.com>
2023-06-23 10:42:39 -0700

    initial commit - attempt to use Tailwind CLI

---
+++ flask_app/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    ".flask_app/templates/**/*.html",
+    ".flask_app/static/src/**/*.css",
+    ".flask_app/static/css/**/*.css",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
